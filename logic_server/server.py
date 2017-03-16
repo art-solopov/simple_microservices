306d83ab01dc5eed01d0ac36b164b73b346d76c0
Artemiy Solopov <art-solopov@yandex.ru>
2017-03-17 01:26:57 +0300

    Basic working server

--- logic_server/server.py
+++ logic_server/server.py
@@ -1,6 +1,45 @@
 import pika
 
-def on_open(connection):
-    print("Connection activated")
 
-connection = pika.SelectConnection(on_open_callback=on_open)
+# Global channel variable
+channel = None
+queue = 'test'
+
+
+def on_connected(connection):
+    print("Connection activated")
+    connection.channel(on_channel_open)
+
+
+def on_channel_open(new_channel):
+    print("Channel opened")
+    global channel
+    channel = new_channel
+    # TODO: queue params
+    channel.queue_declare(
+        queue=queue,
+        durable=True,
+        exclusive=False,
+        auto_delete=False,
+        callback=on_queue_declared
+    )
+
+
+def on_queue_declared(frame):
+    print("Queue {0} declared".format(queue))
+    channel.basic_consume(handle_delivery, queue=queue)
+
+
+def handle_delivery(channel, method, header, body):
+    print(body)
+
+
+conn_params = pika.ConnectionParameters()
+connection = pika.SelectConnection(conn_params, on_connected)
+
+if __name__ == '__main__':
+    try:
+        connection.ioloop.start()
+    except KeyboardInterrupt:
+        connection.close()
+        connection.ioloop.start()
